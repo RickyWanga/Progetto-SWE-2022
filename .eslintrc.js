bbfc6830844373c81397f26095e8825d11d47d96
Carmine Olivo <carmine.olivo@studio.unibo.it>
2021-11-05 17:14:30 +0100

    TG10 - Setup framework #in-progress

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+		requireConfigFile: false
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended'
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		'comma-dangle': 'off',
+		'indent': ['error', 'tab'],
+		'no-tabs': 'off',
+		'space-before-function-paren': 'off',
+		'quote-props': 'off',
+		'vue/html-indent': ['error', 'tab'],
+		'vue/singleline-html-element-content-newline': 'off',
+	}
+}
