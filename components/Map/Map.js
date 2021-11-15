8af8f1baf230ed3c1002be4a22e28aa5664c61ab
emauele.corsi3 <emanuele.corsi3@studio.unibo.it>
2021-11-15 20:24:14 +0100

    MAPPA

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,8 +1,30 @@
+import Vue from 'vue'
+import { LMap, LTileLayer } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+Vue.component('l-map', LMap)
+Vue.component('l-tile-layer', LTileLayer)
+
 export default {
 	name: 'Map',
+	el: '#vue-app',
+	components: {
+		LMap,
+		LTileLayer
+	},
 	data () {
 		return {
-			loading: false,
+			url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+			attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+			zoom: 1,
+			center: [0, 0],
+			isShow: true,
+			loading: true
 		}
 	},
+	mounted() {
+		this.loading = true
+		setTimeout(() => {
+			this.loading = false
+		}, 1200)
+	}
 }
--- components/Map/Map.vue
+++ components/Map/Map.vue
@@ -1,5 +1,9 @@
 <template>
-	<v-container fill-height class="red" />
+	<v-container fill-height>
+		<l-map :zoom="zoom" :center="center">
+			<l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+		</l-map>
+	</v-container>
 </template>
 
 <script src="./Map.js"></script>
