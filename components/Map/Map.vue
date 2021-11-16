ecc0b57d641727ee1e7665f82ae2570cac3d69d0
Carmine Olivo <carmine.olivo@studio.unibo.it>
2021-11-16 10:24:43 +0100

    vue leaflet <- nuxt leaflet

--- components/Map/Map.vue
+++ components/Map/Map.vue
@@ -1,8 +1,10 @@
 <template>
-	<v-container fill-height>
-		<l-map :zoom="zoom" :center="center">
-			<l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-		</l-map>
+	<v-container fill-height class="pa-0">
+		<client-only>
+			<l-map v-if="show" ref="map" :zoom="zoom" :center="center">
+				<l-tile-layer :url="url" :attribution="attribution" />
+			</l-map>
+		</client-only>
 	</v-container>
 </template>
 
