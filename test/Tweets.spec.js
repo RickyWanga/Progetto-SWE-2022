a0255c620e540b07042e92a6a98f97dbda6bce47
Carmine Olivo <carmine.olivo@studio.unibo.it>
2021-11-24 10:09:17 +0100

    Unit test tweets search form

--- test/Tweets.spec.js
+++ test/Tweets.spec.js
@@ -1,14 +1,56 @@
-import { createLocalVue, shallowMount } from "@vue/test-utils"
+import Vuetify from "vuetify"
+import { createLocalVue, mount } from "@vue/test-utils"
 import Component from "@/components/Tweets/Tweets.vue"
 
-describe( "Tweets", () => {
+describe( "Tweets Search Form", () => {
 	const local_vue = createLocalVue()
-	const mountComponent = ( custom_options = {} ) => shallowMount( Component, Object.assign({
+	const mountComponent = ( custom_options = {} ) => mount( Component, Object.assign({
 		local_vue,
+		vuetify,
 	}, custom_options ))
 
+	let vuetify
+	let wrapper
+	let tweetsForm
+	let tweetsFormInput
+
+	beforeAll(() => {
+		vuetify = new Vuetify()
+	})
+
+	beforeEach(() => {
+		wrapper = mountComponent({
+			mocks: {
+				$nuxt: {
+					$emit: () => {},
+				},
+			},
+			propsData: {
+				tweets: []
+			},
+		})
+		tweetsForm = wrapper.find( ".tweets-form" )
+		tweetsFormInput = wrapper.find( ".tweets-form .v-input" )
+	})
+
 	test( "is a Vue instance", () => {
-		const wrapper = mountComponent()
 		expect( wrapper.vm ).toBeTruthy()
+	})
+
+	test( "tweets form input validation", async () => {
+		expect( wrapper.vm.valid ).toBeFalsy()
+
+		await tweetsFormInput.find( "input" ).setValue( "test" )
+		await tweetsFormInput.trigger( "brur" )
+		expect( wrapper.vm.valid ).toBeTruthy()
+	})
+
+	test( "tweets form submit", async () => {
+		const spySubmit = jest.spyOn( wrapper.vm, "submit" )
+		const test_query = "test"
+		await tweetsFormInput.find( "input" ).setValue( test_query )
+		await tweetsForm.trigger( "submit" )
+		expect( spySubmit ).toHaveBeenCalled()
+		expect( test_query === wrapper.vm.query ).toBeTruthy()
 	})
 })
