ee2d90378758f3c5cd10d5236aeada69218e845e
Carmine Olivo <carmine.olivo@studio.unibo.it>
2021-11-26 14:38:09 +0100

    Fix typo

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const scanner = require( "sonarqube-scanner" )
 require( "dotenv" ).config()
 scanner({ options: {
-	"sonar.host.url": process.env.SONAR_HOST_URL
-	"sonar.login": process.env.SONAR_LOGIN
+	"sonar.host.url": process.env.SONAR_HOST_URL,
+	"sonar.login": process.env.SONAR_LOGIN,
 }}, () => process.exit())
