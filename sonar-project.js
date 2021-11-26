6182a0c407d95677199da97781c6b8c7ea127783
Carmine Olivo <carmine.olivo@studio.unibo.it>
2021-11-26 14:24:47 +0100

    Ottimizzazioni CI

--- sonar-project.js
+++ sonar-project.js
@@ -1,5 +1,6 @@
 const scanner = require( "sonarqube-scanner" )
 require( "dotenv" ).config()
 scanner({ options: {
+	"sonar.host.url": process.env.SONAR_HOST_URL
 	"sonar.login": process.env.SONAR_LOGIN
 }}, () => process.exit())
