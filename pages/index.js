53815cc8190d56e5514b35b97cda332c3ddc6f81
Carmine Olivo <carmine.olivo@studio.unibo.it>
2021-12-11 19:07:59 +0100

    frontend stream

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
 import DateGroups from "~/classes/DateGroups"
 import Raccoglitore from "~/classes/Raccoglitore"
+import Stream from "~/classes/Stream"
 
 const CLIENT_CONFIGURATION_ROUTE = "/api/client-configuration"
 const SEARCH_ROUTE = "/api/twitter/search"
 const SENTIMENT_ROUTE = "/api/sentiment"
+const STREAM_ROUTE = "/api/twitter/stream"
+
 const LABEL_INFO_EMPTY = "Sorry, there are no results for this search"
 const LABEL_ERROR_UNKNOWN = "Unknown error"
 
 export default {
-	name: "Dashboard",
 	data() {
 		return {
 			alert: {
 				message: "",
 				type: "",
 				show: false,
+			},
+			http_config: {
+				module: this.$axios,
 			},
 			loading_sentiments: false,
 			loading_tweets: false,
@@ -23,9 +28,14 @@ export default {
 			show_map: true,
 			show_media: true,
 			show_tagcloud: true,
+			stream: {
+				active: false,
+				module: null,
+				query: "",
+			},
+			tweets: [],
 			tweet_modal_show: false,
 			tweet_modal_tweet: null,
-			tweets: [],
 		}
 	},
 	async asyncData({ $axios }) {
@@ -72,10 +82,16 @@ export default {
 	},
 	mounted() {
 		this.$nuxt.$on( "query", this.onQuery )
+		this.$nuxt.$on( "toggle-stream", this.onToggleStream )
 		this.$nuxt.$on( "tweet-click", this.onTweetClick )
 		this.onToggle( "toggle-map", "show_map" )
 		this.onToggle( "toggle-media", "show_media" )
 		this.onToggle( "toggle-tagcloud", "show_tagcloud" )
+
+		this.stream.module = new Stream( STREAM_ROUTE, this.http_config, ( async_data ) => {
+			const tweets = new Raccoglitore( async_data )
+			this.tweets = tweets.list.concat( this.tweets )
+		}, this.showAlertError )
 	},
 	methods: {
 		getSentimentAsync( tweet ) {
@@ -115,6 +131,7 @@ export default {
 				this.tweets = raccoglitore.tweets
 				if ( raccoglitore.tweets.length ) {
 					this.setSentimentsAsync()
+					this.setStreamQuery( query )
 				} else {
 					this.showAlertInfo( LABEL_INFO_EMPTY )
 				}
@@ -126,6 +143,15 @@ export default {
 			this.$nuxt.$on( event, ( toggle ) => {
 				this[ model ] = toggle
 			})
+		},
+		onToggleStream( start ) {
+			if ( start ) {
+				this.stream.module?.start( this.stream.query )
+				this.stream.query = null
+			} else {
+				this.stream.module?.stop()
+			}
+			this.stream.active = start
 		},
 		onTweetClick( tweet ) {
 			this.tweet_modal_show = true
@@ -155,6 +181,14 @@ export default {
 				setTimeout( this.setSentimentsAsync, page_interval, page + 1 )
 			} else {
 				this.loading_sentiments = false
+			}
+		},
+		setStreamQuery( query ) {
+			if ( this.stream.active ) {
+				this.stream.module?.setQuery( query )
+				this.stream.query = null
+			} else {
+				this.stream.query = query
 			}
 		},
 		showAlert( message, type, show = true ) {
